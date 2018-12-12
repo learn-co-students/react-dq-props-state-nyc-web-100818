1936bf4a7888ef3e26a7b84a31072c7984993399
magic-onion <alexander.m.truong@gmail.com>
2018-12-12 10:11:10 -0500

    Done.

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -5,42 +5,85 @@ import SunBaby from './assets/sun-eyes.png'
 import BlueBaby from './assets/blue-eyes.png'
 import GlowingBaby from './assets/glowing-eyes.png'
 
-// is there a way we could associate eye color string values with images? 
-// perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
 export default class BabyHog extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      weight: 500,
+      img: normalBaby
+    }
   }
 
   changeWeight = (e) => {
-    // nothing needs to change here
     const newWeight = e.target.name === "+" ? (this.state.weight + 10) : (this.state.weight - 10)
     this.setState({
       weight: newWeight
     })
   }
 
+  componentDidUpdate = (prevProps) => {
+    if (prevProps !== this.props) {
+      switch (this.props.eyeColor) {
+        case "blue":
+          this.setState({
+            img: BlueBaby
+          });
+          break;
+        case "sun":
+          this.setState({
+            img: SunBaby
+          });
+          break;
+        case "glowing":
+          this.setState({
+            img: GlowingBaby
+          });
+          break;
+        default:
+          console.log("nah bro")
+      }
+    }
+  }
+
+
+  handleWeight = (event) => {
+    switch (event.target.name) {
+      case "+":
+        this.setState({
+          weight: this.state.weight + 10
+        });
+        break;
+      case "-":
+        this.setState({
+          weight: this.state.weight - 10
+        });
+        break;
+      default:
+        console.log("nah bro")
+    }
+  }
+
   render() {
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
-          
-        <Button name="+">
+        <h1>Name: {this.props.details.name}</h1>
+        <h3>Weight: {this.state.weight} kg</h3>
+        <h3>Hobby: {this.props.details.name}</h3>
+        <h4>Eye Color: {this.props.eyeColor}</h4>
+
+        <Button name="+" onClick={this.handleWeight}>
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button name="-" onClick={this.handleWeight}>
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={this.state.img} style={{height: '200px'}} alt="MasterBlasterJrJr" />
         </div>
-        
+
       </li>
     )
   }
