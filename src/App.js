570c87a1c8c67c87a27d2c94c171f0839b4dc9b2
sachafrosell <sachafrosell@gmail.com>
2018-12-12 11:46:25 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import MasterHog from './MasterHog'
 import './App.css'
+// import './db.js'
 
 export default class App extends Component {
-  
+
   render() {
     return (
       <div className="App">
